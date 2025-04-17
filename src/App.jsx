bba1d9e94f5f85f15e8f0e928763c5bc0cbecd70
creamymood <wbwbwlgid@naver.com>
2025-04-17 11:25:34 +0900

    header, content, app 부분을 수정 scss문법중 변수랑 믹스인 부분을 스타일컴포넌트로 변환

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
+import styled from "styled-components";
 import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import {flex} from "./styled/styled"
+
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <Section>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </Section>
     </main>
   );
 }
 
 export default App;
+
+
+const Section = styled.section `
+  ${flex({justify: 'center', align: 'center', wrap: 'wrap', gap: '20px'})};
+  padding: 20px 40px;
+
+`
+
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,36 @@
+import styled from "styled-components";
+import {flex, font} from "../styled/styled"
+
+
 export default function Header() {
   return (
-    <header className="header-container">
+    <Headerstyle>
       <h1>OZ코딩스쿨</h1>
       <ul className="menu-container">
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
       </ul>
-    </header>
+    </Headerstyle>
   );
 }
+
+
+const Headerstyle = styled.header `
+  
+  ${flex({justify: 'space-between', align: 'center'})};
+  padding: 20px 20px;
+  background-color: black;
+  ul {
+    ${flex({justify: 'center', align: 'center', gap: '20px'})};
+    li {
+      ${font({size: '16px', weight: '400'})};
+      list-style: none;
+    }
+  }
+
+`
+
+
+
+
